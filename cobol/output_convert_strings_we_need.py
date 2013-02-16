5eec71eb75c6094055ad12de246a942ae2ed07c8
Travis Beatty <travisby@gmail.com>
2013-02-16 15:35:16 -0500

    Created a cobol generator for the converting in python :)

---
+++ cobol/output_convert_strings_we_need.py
@@ -0,0 +1,25 @@
+def wraparound_str(string, startPoint):
+    new_str = []
+    length = len(string)
+    for i in range(length):
+        new_str.append(string[(startPoint + i) % length])
+    return ''.join(new_str)
+
+
+
+def make_str(var, inp, outp):
+    return (
+        "INSPECT %(var)s CONVERTING %(input)s TO %(output)s" %
+        {'var': var, 'input': inp, 'output': outp}
+    )
+
+def main():
+    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    str_name = raw_input("Please enter the name you called the input string: ")
+    print make_str(str_name, ALPHABET, alphabet)
+    for i in range(len(alphabet)):
+        print make_str(str_name, alphabet, wraparound_str(alphabet, i))
+
+main()
