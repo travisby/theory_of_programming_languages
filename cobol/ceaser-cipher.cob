de418c8da96738bfb80b16be8352e235aa6c7ff9
Travis Beatty <travisby@gmail.com>
2013-02-16 16:10:21 -0500

    Made simple accept string for cobol

--- cobol/ceaser-cipher.cob
+++ cobol/ceaser-cipher.cob
@@ -3,3 +3,8 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 User-String  PIC A(50).
+       PROCEDURE DIVISION.
+           DISPLAY "Enter string: " WITH NO ADVANCING.
+           ACCEPT User-String
+           DISPLAY  "Your Input String = ", User-String
+           STOP RUN.
