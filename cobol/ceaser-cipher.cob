b08e3a384dac0a3e40ae73dd31f4871aa7b290a3
Travis Beatty <travisby@gmail.com>
2013-02-16 16:22:46 -0500

    lower cased the input

--- cobol/ceaser-cipher.cob
+++ cobol/ceaser-cipher.cob
@@ -3,8 +3,9 @@
        DATA DIVISION.
        LOCAL-STORAGE SECTION.
        01 User-String  PIC A(50).
+       01 Converted-String  PIC A(50).
        PROCEDURE DIVISION.
            ACCEPT User-String FROM ARGUMENT-VALUE
-           DISPLAY  "Your Input String = ", User-String
+           DISPLAY  " = ", FUNCTION LOWER-CASE( User-String )
 
            STOP RUN.
