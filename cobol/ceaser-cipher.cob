efe3916be461146c98f18e9f81d1e3cff990d83c
Travis Beatty <travisby@gmail.com>
2013-02-16 16:17:57 -0500

    Have cobol get the parameter from the command line!

--- cobol/ceaser-cipher.cob
+++ cobol/ceaser-cipher.cob
@@ -1,10 +1,10 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CEASER-CIPHER.
        DATA DIVISION.
-       WORKING-STORAGE SECTION.
+       LOCAL-STORAGE SECTION.
        01 User-String  PIC A(50).
        PROCEDURE DIVISION.
-           DISPLAY "Enter string: " WITH NO ADVANCING.
-           ACCEPT User-String
+           ACCEPT User-String FROM ARGUMENT-VALUE
            DISPLAY  "Your Input String = ", User-String
+
            STOP RUN.
