07cc6f1dae6eba51f8c68bd253bf9cb5f9cdfeab
Travis Beatty <travisby@gmail.com>
2013-03-30 10:45:54 -0400

    Changed the accidental recursive call in encrypt to encrypt_one

--- erlang/ceaser.erl
+++ erlang/ceaser.erl
@@ -1,7 +1,7 @@
 -module(ceaser).
 -export([encrypt/2, decrypt/2]).
 
-encrypt(Str, Key) -> lists:map(fun(Char) -> encrypt(Char, Key) end, Str).
+encrypt(Str, Key) -> lists:map(fun(Char) -> encrypt_one(Char, Key) end, Str).
 decrypt(Str, Key) -> encrypt(Str, -Key).
 
 encrypt_one(Char, Key) -> Char.
