cbe259d0264a85747f808d157c382f92d52a8459
Travis Beatty <travisby@gmail.com>
2013-03-30 10:42:19 -0400

    Setup the high level piece of our erlang solution

--- erlang/ceaser.erl
+++ erlang/ceaser.erl
@@ -1,7 +1,7 @@
 -module(ceaser).
 -export([encrypt/2, decrypt/2]).
 
-encrypt(Str, Key) -> lists:map(fun(X) -> X end, Str).
+encrypt(Str, Key) -> lists:map(fun(Char) -> encrypt(Char, Key) end, Str).
 decrypt(Str, Key) -> encrypt(Str, -Key).
 
 encrypt_one(Char, Key) -> Char.
