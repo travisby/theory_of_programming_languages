9db2a0fc864194d275ad7e74a9fe1f19f90ebf47
Travis Beatty <travisby@gmail.com>
2013-04-22 09:43:48 -0400

    Created list of lists of alphabets with python to add into lisp

---
+++ lisp/generate_lists.py
@@ -0,0 +1,24 @@
+def wraparound_str(string, startPoint):
+    new_str = []
+    length = len(string)
+    for i in range(length):
+        new_str.append(string[(startPoint + i) % length])
+    return ''.join(new_str)
+
+def make_str(var, inp, outp):
+    return (
+        outp
+    )
+
+def main():
+    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    print "(DEFUN alphabets ()"
+    for i in range(len(alphabet)):
+        print '\t("%s")' % wraparound_str(alphabet, i)
+
+    print ")"
+
+
+main()
