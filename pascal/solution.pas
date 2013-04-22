ad80e45874c8cdbc2580b65b4ff62168b80dad4b
Travis Beatty <travisby@gmail.com>
2013-04-22 14:43:58 -0400

    Added base pascal file

---
+++ pascal/solution.pas
@@ -0,0 +1,12 @@
+program Ceaser(output);
+    procedure encrypt(ourString: String; key: Integer);
+        begin
+        end;
+    procedure decrypt(ourString: String; key: Integer);
+        begin
+        end;
+    procedure solve(ourString: String);
+        begin
+        end;
+begin
+end.
