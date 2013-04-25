7630250fb793b0c4f49568c5f3ea203ddb77184f
Travis Beatty <travisby@gmail.com>
2013-04-24 22:31:31 -0400

    Fixed our decrypt function for erlang

--- erlang/ceaser.erl
+++ erlang/ceaser.erl
@@ -2,7 +2,7 @@
 -export([encrypt/2, decrypt/2]).
 
 encrypt(Str, Key) -> lists:map(fun(Char) -> encrypt_one(Char, Key) end, Str).
-decrypt(Str, Key) -> encrypt(Str, -Key).
+decrypt(Str, Key) -> encrypt(Str, 26 - Key).
 
 % erlang is EVIL for not allowing any function in guards...
 encrypt_one(Char, Key) when ($A =< Char) and (Char =< $z) ->
