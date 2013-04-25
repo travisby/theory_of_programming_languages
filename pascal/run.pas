2f9728108624176e271b3fb8b1c1caec7c20230e
Travis Beatty <travisby@gmail.com>
2013-04-25 10:12:02 -0400

    Made pascal handle CLI arguments

--- pascal/run.pas
+++ pascal/run.pas
@@ -1,5 +1,9 @@
+
 program Ceaser(output);
+Uses sysutils;
+    var func : String;
     var ourStr : String;
+    var key    : Integer;
     var encrypted : String;
     var decrypted : String;
     var solved : String;
@@ -22,6 +26,7 @@ program Ceaser(output);
                     if (ord(ourString[i]) >= upperA) and (ord(ourString[i]) <= upperZ) then
                         encrypt := encrypt + (chr((upperA + ((ord(ourString[i]) - upperA + key) mod 26))))
                     else if (ord(ourString[i]) >= lowerA) and (ord(ourString[i]) <= lowerZ) then
+                        encrypt := encrypt + (chr((lowerA + ((ord(ourString[i]) - lowerA + key) mod 26))))
                     else
                         encrypt := encrypt;
                     i := i + 1;
@@ -47,12 +52,17 @@ program Ceaser(output);
         end;
 
     begin
-        ourStr := 'HAL';
-        encrypted := encrypt(ourStr, 1);
-        decrypted := decrypt(encrypted, 1);
-        solved := solve(encrypted);
-        writeln(ourStr);
-        writeln(encrypted);
-        writeln(decrypted);
-        writeln(solved);
+        func := ParamStr(1);
+        ourStr := ParamStr(2);
+        if (func = 'encrypt') or (func = 'decrypt') then
+            key := StrToInt(ParamStr(3))
+        else
+            ;
+        if func = 'encrypt' then
+            writeln(encrypt(ourStr, key))
+        else if func = 'decrypt' then
+            writeln(decrypt(ourStr, key))
+        else
+            writeln(solve(ourStr));
+
     end.
