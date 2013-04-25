9e4086c0048a9a988410b2fa732221df81bccc56
Travis Beatty <travisby@gmail.com>
2013-04-25 11:48:58 -0400

    Created python script to test all the things

---
+++ test.py
@@ -0,0 +1,34 @@
+import os
+
+
+test_cases = {
+    'encrypt "hal" 1': 'ibm',
+    'encrypt "HAL" 1': 'IBM',
+    'decrypt "ibm" 1':  'hal',
+    'decrypt "IBM" 1':  'HAL',
+    'encrypt "words with spaces"  5': "nfiuj nzky jgrtvj",
+}
+directories = [
+    'cobol',
+    'erlang',
+    'fortran',
+    'lisp',
+    'ml',
+    'pascal',
+    'scala',
+]
+
+for directory in directories:
+    os.chdir(directory)
+    for case in test_cases:
+        pointer = os.popen('./run %s' % case)
+        result = pointer.read()
+        if test_cases[case] not in result:
+            print '---------------------'
+            print 'Fail!'
+            print directory
+            print case
+            print 'expected: %s' % test_cases[case]
+            print 'output: %s' % result
+            print '---------------------'
+    os.chdir('../')
